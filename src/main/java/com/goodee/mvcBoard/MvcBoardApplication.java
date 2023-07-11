08cb4f9ac48e11c6e14fcec97f34cff8726c62b2
ueuye <eu99ne@gmail.com>
2023-07-11 16:08:07 +0900

    프로젝트 초기화

---
+++ src/main/java/com/goodee/mvcBoard/MvcBoardApplication.java
@@ -0,0 +1,13 @@
+package com.goodee.mvcBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodee/mvcBoard/MvcBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodee.mvcBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
